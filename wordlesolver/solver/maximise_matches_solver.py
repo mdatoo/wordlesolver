db93df95b3cc421a93d8cfda9377ed6e84df972d
Mikhail Datoo <mikhail@datoos.com>
2022-11-20 12:50:21 +0000

    refactor: cleaned up solver class

--- wordlesolver/solver/maximise_matches_solver.py
+++ wordlesolver/solver/maximise_matches_solver.py
@@ -5,39 +5,16 @@ Classes:
     MaximiseMatchesSolver(Solver)
 """
 
-from logging import info
 from typing import List
 
 from tqdm import tqdm
 
-from ..data import POSSIBLE_WORDS
 from .solver import Solver
 
 
 # pylint: disable=too-few-public-methods; runnable solver
 class MaximiseMatchesSolver(Solver):
     """Maximise matches solver class."""
-
-    def run(self) -> None:
-        """Run solver."""
-        self._generator.reset()
-
-        done = False
-        possible_words = POSSIBLE_WORDS
-
-        while not done:
-            next_guess = self._next_guess(possible_words)
-            _, reward, done, extra_data = self._generator.step(
-                POSSIBLE_WORDS.index(next_guess)
-            )
-            possible_words = extra_data["possible_words"]
-            word_validity = extra_data["word_validity"]
-
-            info(
-                f"Guessed {next_guess}, got reward {reward}, got word validity {word_validity}"
-            )
-
-        info("Finished game")
 
     def _next_guess(self, possible_words: List[str]) -> str:
         return str(
--- wordlesolver/solver/solver.py
+++ wordlesolver/solver/solver.py
@@ -6,7 +6,10 @@ Classes:
 """
 
 from abc import ABC, abstractmethod
+from logging import info
+from typing import List
 
+from ..data import POSSIBLE_WORDS
 from ..generator import Generator
 
 
@@ -27,7 +30,27 @@ class Solver(ABC):
         """Initialise object."""
         self._generator = generator
 
-    @abstractmethod
     def run(self) -> None:
         """Run the game."""
+        self._generator.reset()
+
+        done = False
+        possible_words = POSSIBLE_WORDS
+
+        while not done:
+            next_guess = self._next_guess(possible_words)
+            _, reward, done, extra_data = self._generator.step(
+                POSSIBLE_WORDS.index(next_guess)
+            )
+            possible_words = extra_data["possible_words"]
+            word_validity = extra_data["word_validity"]
+
+            info(
+                f"Guessed {next_guess}, got reward {reward}, got word validity {word_validity}"
+            )
+
+        info("Finished game")
+
+    @abstractmethod
+    def _next_guess(self, possible_words: List[str]) -> str:
         raise NotImplementedError
